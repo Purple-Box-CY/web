e16a528ed28af72177f31790ab856f237eb6e0af
Danil Larin <pickledan@yandex.ru>
2024-11-23 19:08:39 +0200

    feat: implement navigation

---
+++ src/pages/camera.tsx
@@ -0,0 +1,13 @@
+import Menu from "../components/menu/menu";
+import React from "react";
+
+const CameraPage = () => {
+  return (
+    <div className={"h-svh bg-white"}>
+      Camera!
+      <Menu />
+    </div>
+  );
+};
+
+export default CameraPage;
