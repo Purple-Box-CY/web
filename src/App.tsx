e3b9cf523ab87cd169983b74a0c8fca5414e0ae6
Danil Larin <pickledan@yandex.ru>
2024-11-23 11:18:51 +0200

    feat: connect map

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import MapComponent from "./components/map";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">Purple box</header>
+    <div>
+      <MapComponent />
     </div>
   );
 }
