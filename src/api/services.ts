80ad15efc1f25a6267d13a04a18d4bcc58aca029
1JigSaW <jigsaw11211@gmail.com>
2024-11-24 01:25:07 +0200

    camera recognition page

--- src/api/services.ts
+++ src/api/services.ts
@@ -9,3 +9,31 @@ export const service = {
     return await publicApi.get(`/articles/${alias}`);
   },
 };
+
+export const camera = {
+  getRecognitionItem: async (imageBase64: string) => {
+    const base64ToBlob = (base64: string) => {
+      const byteString = atob(base64.split(",")[1]);
+      const mimeString = base64.split(",")[0].split(":")[1].split(";")[0]; // Определяем MIME тип
+      const arrayBuffer = new ArrayBuffer(byteString.length);
+      const intArray = new Uint8Array(arrayBuffer);
+
+      for (let i = 0; i < byteString.length; i++) {
+        intArray[i] = byteString.charCodeAt(i);
+      }
+
+      return new Blob([intArray], { type: mimeString });
+    };
+
+    const imageBlob = base64ToBlob(imageBase64);
+
+    const formData = new FormData();
+    formData.append("file", imageBlob, "image.jpg");
+
+    return await publicApi.post("/camera/box", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+  },
+};
