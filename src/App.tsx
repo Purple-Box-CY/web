9f1d15e79e155f7d5c8c39d3882d057cec72f8fd
1JigSaW <jigsaw11211@gmail.com>
2024-11-23 18:50:26 +0200

    add page for photo

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MapComponent from "./components/map/map";
+import RecognitionBox from "./pages/RecognitionBoxPage";
 
 function App() {
-  return (
-    <div>
-      <MapComponent />
-    </div>
-  );
+    return (
+        <Router>
+            <div>
+                <Routes>
+                    <Route path="/" element={<MapComponent />} />
+                    <Route path="/recognition-box" element={<RecognitionBox />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
