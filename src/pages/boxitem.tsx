29380b9973dacbde1494c0b1b1c02edcf8cb273f
Danil Larin <pickledan@yandex.ru>
2024-11-23 19:06:40 +0200

    feat: implement navigation

---
+++ src/pages/boxitem.tsx
@@ -0,0 +1,13 @@
+import Menu from "../components/menu/menu";
+import React from "react";
+
+const BoxItem = () => {
+  return (
+    <div className={"h-svh"}>
+      Info!
+      <Menu />
+    </div>
+  );
+};
+
+export default BoxItem;
---
+++ src/pages/profile.tsx
@@ -0,0 +1,13 @@
+import Menu from "../components/menu/menu";
+import React from "react";
+
+const ProfilePage = () => {
+  return (
+    <div className={"h-svh bg-white"}>
+      Profile!
+      <Menu />
+    </div>
+  );
+};
+
+export default ProfilePage;
