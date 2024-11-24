b7c97894223ada2b7b4846062288e63bb2f58d3f
1JigSaW <jigsaw11211@gmail.com>
2024-11-24 03:17:37 +0200

    fix

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,10 +1,16 @@
 import axios from "axios";
 
 export const baseURL = process.env.REACT_APP_API_URL;
+export const baseURL2= process.env.REACT_APP_API_URL_2;
 
 export const publicApi = axios.create({
-  baseURL,
+    baseURL,
 });
+
+export const publicApi2 = axios.create({
+    baseURL: baseURL2,
+});
+
 
 
 publicApi.interceptors.response.use(
